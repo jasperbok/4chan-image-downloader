c431fd9ce061254f78b1c964c3020a5d6b637b6e
lunanoko <hello@jasperbok.nl>
2012-10-23 17:42:36 +0200

    Updated README and OOP'ed most of the app.
    I changed the bulk of the program's code to an object oriented style which
    should make it easier to extend it in the coming days. Also makes it a bit
    easier to extend the program by yourself.

--- download_images.py
+++ download_images.py
@@ -1,21 +1,119 @@
 import os
 import subprocess
 import sys
+import re
+import urllib
+
+class ImageBoardDownloader:
+    """Finds images and downloads them from 4Chan."""
+
+    def __init__(self):
+        self.setup()
+        self.board = raw_input('What board should I download from?\n')
+        self.tread = raw_input('What tread number?\n')
+        self.base_url = self.make_url(self.board, self.tread)
+        self.directory = self.make_directory()
+        self.image_urls = self.find_image_urls(self.base_url)
+        self.remove_duplicates()
+        self.download_images()
+
+    def setup(self):
+        self.name = 'Dummy'
+
+    def report(self):
+        """
+        Returns a single line in which the current status of the downloader
+        is shown.
+
+        """
+        return 'Awaiting user input...' 
+
+    def make_url(self, board, tread):
+        """Return the absolute URL derived from the board and tread number."""
+        return 'http://boards.4chan.org/' + board + '/res/' + tread
+
+    def make_directory(self):
+        """
+        Creates the directory path where the images will be saved. If the
+        directory does not exist it will be created.
+        
+        TO-DO:
+        alias support
+
+        """
+        directory = os.path.join(os.getcwd(), self.name, self.board, self.tread)
+
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        return directory
+        # download_images(check_file_duplicates(img_urls))
+
+    def find_image_urls(self, url):
+        """
+        Finds all the images that should be downloaded.
+
+        """
+        img_urls = []
+        html = urllib.urlopen(url).read()
+        tmp_urls = re.findall('(/[A-Za-z]+/src/\d+\.)(jpeg|jpg|png|gif)', html)
+
+        for img in tmp_urls:
+            img_urls.append('http://images.4chan.org' + img[0] + img[1])
+
+        return list(set(img_urls))
+
+    def remove_duplicates(self):
+        """
+        Removes URLs from the self.image_urls list that were previously
+        downloaded.
+
+        """
+        dupes = []
+
+        for file in self.image_urls:
+            if os.path.isfile(os.path.join(self.directory, file.split('/')[-1])):
+                dupes.append(file)
+
+        for file in dupes:
+            self.image_urls.remove(file)
+
+        print str(len(dupes)) + ' Files already exist on disk'
+
+    def download_images(self):
+        """
+        Download all images from the URLs stored in self.image_urls.
+        
+        """
+        total_num_images = len(self.image_urls)
+        current_image = 1
+
+        for url in self.image_urls:
+            print 'DOWNLOADING IMAGE ' + str(current_image) + ' OUT OF ' + str(total_num_images)
+            subprocess.call(['wget', '--directory-prefix=' + self.directory, url])
+            current_image = current_image + 1
+
+class FourChanDownloader(ImageBoardDownloader):
+    def __init__(self):
+        self.name = '4chan'
+
+    def make_url(self, board, tread):
+        """Return the absolute URL derived from the board and tread number."""
+        return 'http://boards.4chan.org/' + board + '/res/' + tread
+
+daemons = []
 
 def initialize():
-    start_dir = os.getcwd()
-    website = raw_input('From what board are we going to download?\n\n1 - 4chan\n2 - desuchan\n')
+    website = raw_input('From what website are we going to download?\n\n1 - 4chan\n2 - desuchan\n')
     if website == '1':
-        website = '4chan'
-        import fourchan_helper as helper
+        daemons.append(FourChanDownloader())
     elif website == '2':
         import desuchan_helper as helper
         website = 'desuchan'
     else:
         print 'That\'s not a valid option. Please choose something else.'
         initialize()
-    board = raw_input('What board are we going to download from?\n')
-    tread = raw_input('What tread are we going to download?\n')
+    '''
     tread_alias = ''
     with open('tread_aliases.txt', 'r') as alias_file:
         for line in alias_file:
@@ -37,31 +135,10 @@ def initialize():
                     alias_file.write(contents + '\n' + tread + '=' + tread_alias)
             else:
                 tread_alias = tread
-    url = helper.make_url(board, tread)
+    '''
     print 'We\'re going to download from ' + url
     img_urls = helper.find_image_urls(url)
 
-    if not os.path.exists(os.path.join(start_dir, website, board, tread_alias)):
-        os.makedirs(os.path.join(start_dir, website, board, tread_alias))
-
-    os.chdir(os.path.join(start_dir, website, board, tread_alias))
-    download_images(check_file_duplicates(img_urls))
-    os.chdir(start_dir)
-
-def download_images(urls):
-    """Download all images in a list.
-
-    Keyword arguments:
-    urls -- a list containing image URLs
-
-    """
-    total_num_images = len(urls)
-    current_image = 1
-
-    for url in urls:
-        print 'DOWNLOADING IMAGE ' + str(current_image) + ' OUT OF ' + str(total_num_images)
-        subprocess.call(['wget', url])
-        current_image = current_image + 1
 
 def check_file_duplicates(files, directory=""):
     dupes = []
